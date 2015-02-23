5b74ce9b9e364b0629a95cd075315b2b25b1f567
Michael Su <mdasu1@gmail.com>
2015-02-22 23:47:38 -0800

    Temporary fix for tests.
    Need to change data source to non-adjusted source otherwise data will
    keep changing.

--- tests/integration/test_examples.py
+++ tests/integration/test_examples.py
@@ -32,10 +32,10 @@ def test_quickstart():
 
     prophet.register_portfolio_analyzers(default_analyzers)
     analysis = prophet.analyze_backtest(backtest)
-    assert round(analysis['sharpe'], 10) == 1.0970973495
-    assert round(analysis['average_return'], 10) == 0.0010547843
-    assert round(analysis['cumulative_return'], 10) == 2.1688171559
-    assert round(analysis['volatility'], 10) == 0.0152622562
+    assert round(analysis['sharpe'], 10) == 1.1083876014
+    assert round(analysis['average_return'], 10) == 0.0010655311
+    assert round(analysis['cumulative_return'], 10) == 2.2140809296
+    assert round(analysis['volatility'], 10) == 0.0152607097
 
     today = datetime(2014, 11, 10)
     expected_orders = Orders(Order(symbol='AAPL', shares=100))
